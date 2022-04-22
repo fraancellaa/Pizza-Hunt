50945061d07976dfc011898677dc8c0dd3d6cc3c
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-04-22 14:38:54 -0400

    modifying files

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -12,25 +12,25 @@ let pizzaId;
 function getPizza() {
   // get id of pizza
   const searchParams = new URLSearchParams(document.location.search.substring(1));
-
   const pizzaId = searchParams.get('id');
 
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
-  .then(response => {
-    // check for a 4xx or 5xx error from server
-    if (!response.ok) {
-      throw new Error({ message: 'Something went wrong!' });
-    }
+    .then(response => {
+      console.log(response);
+      if (!response.ok) {
+        console.log('hi');
+        throw new Error({ message: 'Something went wrong!' });
+      }
 
-    return response.json();
-  })
-  .then(printPizza)
-  .catch(err => {
-    console.log(err);
-    alert('Cannot find a pizza with this id! Taking you back...');
-    window.history.back();
-  });
+      return response.json();
+    })
+    .then(printPizza)
+    .catch(err => {
+      console.log(err);
+      alert('Cannot find a pizza with this id! Taking you back.');
+      window.history.back();
+    });
 }
 
 function printPizza(pizzaData) {
@@ -120,19 +120,19 @@ function handleNewCommentSubmit(event) {
     },
     body: JSON.stringify(formData)
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Something went wrong!');
-    }
-    response.json();
-  })
-  .then(commentResponse => {
-    console.log(commentResponse);
-    location.reload();
-  })
-  .catch(err => {
-    console.log(err);
-  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      // location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function handleNewReplySubmit(event) {
@@ -161,19 +161,19 @@ function handleNewReplySubmit(event) {
     },
     body: JSON.stringify(formData)
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Something went wrong!');
-    }
-    response.json();
-  })
-  .then(commentResponse => {
-    console.log(commentResponse);
-    location.reload();
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 $backBtn.addEventListener('click', function() {
